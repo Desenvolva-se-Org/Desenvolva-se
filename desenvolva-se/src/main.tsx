29983065e1cdd9e725316c2f03692eb118dd004c
guilhermesolagarcia <guilhermesolagarcia31@gmail.com>
2025-11-18 14:21:31 -0300

    feat: Alterando o App.tsx e main.tsx limpando arquivos inutilizados.

--- desenvolva-se/src/main.tsx
+++ desenvolva-se/src/main.tsx
@@ -1,9 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// Mude esta linha:
-import './index.css' // <-- Remover ou comentar
-// Para esta:
-import './styles/globals.css' // <-- NOVA IMPORTAÇÃO
+import './styles/globals.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
